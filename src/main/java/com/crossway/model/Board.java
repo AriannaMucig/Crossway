dbc0c9271879eb9fc47d8dee8e80c4cc6a8ba0a3
Arianna <ariannamucig@gmail.com>
2026-07-30 16:29:46 +0200

    implementation of Board costructor,  is CellEmpty, getStone and placeStone function

--- src/main/java/com/crossway/model/Board.java
+++ src/main/java/com/crossway/model/Board.java
@@ -1,7 +1,36 @@
 package com.crossway.model;
 
 public class Board {
-    public boolean isEmpty(){
+    private final Cell[][] grid;
+
+    public Board() {
+        this.grid = new Cell[19][19];
+        for (int r = 0; r < 19; r++) {
+            for (int c = 0; c < 19; c++) {
+                grid[r][c] = new Cell();
+            }
+        }
+    }
+
+    public void placeStone(Position pos, PlayerColor color){
+        grid[pos.x()][pos.y()].setColor(color);
+    }
+
+    public boolean isEmpty() {
+        for (int r = 0; r < 19; r++) {
+            for (int c = 0; c < 19; c++) {
+                if(!grid[r][c].isEmpty())
+                    return false;
+            }
+        }
         return true;
     }
+    public boolean isCellEmpty(Position pos){
+        return grid[pos.x()][pos.y()].isEmpty();
+    }
+
+    public PlayerColor getStone(Position pos){
+        return grid[pos.x()][pos.y()].getColor();
+    }
+
 }
