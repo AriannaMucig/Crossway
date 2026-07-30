8fa43ee18558c87e8ed1210e36c1723108afe903
Arianna <ariannamucig@gmail.com>
2026-07-30 15:43:43 +0200

    test empty initial board

---
+++ src/test/java/com/crossway/model/BoardTest.java
@@ -0,0 +1,13 @@
+package com.crossway.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoardTest {
+    @Test
+    void testInitialBoardIsEmpty(){
+        Board board = new Board();
+        assertThat(board.isEmpty()).isTrue();
+    }
+}
