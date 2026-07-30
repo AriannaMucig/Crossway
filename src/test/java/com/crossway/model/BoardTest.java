3757a1833d6772d7a9ebc5749a2776767db5eded
Arianna <ariannamucig@gmail.com>
2026-07-30 16:07:11 +0200

    test place stone inside the board

--- src/test/java/com/crossway/model/BoardTest.java
+++ src/test/java/com/crossway/model/BoardTest.java
@@ -6,9 +6,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BoardTest {
     @Test
-    void testInitialBoardIsEmpty(){
+    void testInitialBoardIsEmpty() {
         Board board = new Board();
         assertThat(board.isEmpty()).isTrue();
+    }
+
+    @Test
+    void testPlaceStoneInsideBoard(){
+        Board board = new Board();
+        Position pos = new Position(1,1);
+
+        board.placeStone(pos, PlayerColor.BLACK);
+        assertThat(board.isCellEmpty(pos)).isFalse();
+        assertThat(board.getStone(pos)).isEqualTo(PlayerColor.BLACK);
     }
 
 }
