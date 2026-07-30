110d6eeb62cac5294dbcd4efa66db68e6703065d
Arianna <ariannamucig@gmail.com>
2026-07-30 16:52:27 +0200

    test for out of board exception

--- src/test/java/com/crossway/model/BoardTest.java
+++ src/test/java/com/crossway/model/BoardTest.java
@@ -3,6 +3,7 @@ package com.crossway.model;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BoardTest {
     @Test
@@ -19,6 +20,16 @@ class BoardTest {
         board.placeStone(pos, PlayerColor.BLACK);
         assertThat(board.isCellEmpty(pos)).isFalse();
         assertThat(board.getStone(pos)).isEqualTo(PlayerColor.BLACK);
+    }
+    @Test
+    void testPlaceStoneOutsideBoardThrowsException(){
+        Board board = new Board();
+        Position pos1 = new Position(1,-11);
+        Position pos2 = new Position(19,1);
+
+
+        assertThrows(IllegalArgumentException.class, () -> board.placeStone(pos1, PlayerColor.BLACK));
+        assertThrows(IllegalArgumentException.class, () -> board.placeStone(pos2, PlayerColor.WHITE));
     }
 
 }
