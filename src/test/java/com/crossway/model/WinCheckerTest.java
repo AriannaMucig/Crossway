8b4f083a36916fb0593164a59554d11fd0fcc274
Arianna <ariannamucig@gmail.com>
2026-08-02 15:59:12 +0200

    test for white wins with a straight line from left to right

--- src/test/java/com/crossway/model/WinCheckerTest.java
+++ src/test/java/com/crossway/model/WinCheckerTest.java
@@ -9,4 +9,14 @@ class WinCheckerTest {
         Game game = new Game();
         assertThat(game.getWinner()).isNull();
     }
+
+    @Test
+    void testWhiteWinsWithStraightLine(){
+        Game game = new Game();
+        for (int i = 0; i < 19; i++) {
+            game.playMove(new Position(i,0));
+            game.playMove(new Position(i,7));
+        }
+        assertThat(game.getWinner()).isEqualTo(PlayerColor.WHITE);
+    }
 }
