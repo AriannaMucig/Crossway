61265114229ad296a65c416bac74c6d0c05fa17d
Arianna <ariannamucig@gmail.com>
2026-08-02 16:12:22 +0200

    test for black wins witth straight line from top to bottom

--- src/test/java/com/crossway/model/WinCheckerTest.java
+++ src/test/java/com/crossway/model/WinCheckerTest.java
@@ -11,12 +11,22 @@ class WinCheckerTest {
     }
 
     @Test
-    void testWhiteWinsWithStraightLine(){
+    void testWhiteWinsWithStraightLine() {
         Game game = new Game();
         for (int i = 0; i < 19; i++) {
-            game.playMove(new Position(i,0));
-            game.playMove(new Position(i,7));
+            game.playMove(new Position(i, 0));
+            game.playMove(new Position(i, 7));
         }
         assertThat(game.getWinner()).isEqualTo(PlayerColor.WHITE);
+    }
+
+    @Test
+    void testBlackWinsWithStraightLine(){
+        Game game = new Game();
+        for (int i = 0; i < 19; i++) {
+            game.playMove(new Position(5,i));
+            game.playMove(new Position(0,i));
+        }
+        assertThat(game.getWinner()).isEqualTo(PlayerColor.BLACK);
     }
 }
