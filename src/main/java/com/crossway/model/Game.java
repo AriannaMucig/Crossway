251396b9b91309b81e707198e013dd545edb7807
Arianna <ariannamucig@gmail.com>
2026-08-02 16:09:39 +0200

    implementation of the win logic

--- src/main/java/com/crossway/model/Game.java
+++ src/main/java/com/crossway/model/Game.java
@@ -1,11 +1,13 @@
 package com.crossway.model;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Game {
     private PlayerColor currentTurn;
     private Board board;
     private int turnsCount;
     private Position firstMovePosition;
-    private PlayerColor winner;
 
     public Game() {
         this.board = new Board();
@@ -19,21 +21,21 @@ public class Game {
 
     public void playMove(Position pos) {
         PlayerColor color = currentTurn;
-        if (turnsCount==1){
-            firstMovePosition= pos;
+        if (turnsCount == 1) {
+            firstMovePosition = pos;
         }
         board.placeStone(pos, color);
         currentTurn = (color == PlayerColor.BLACK) ? PlayerColor.WHITE : PlayerColor.BLACK;
     }
 
-    public void applyPieRule(){
-        if (turnsCount != 1 && currentTurn==PlayerColor.WHITE) {
+    public void applyPieRule() {
+        if (turnsCount != 1 && currentTurn == PlayerColor.WHITE) {
             throw new IllegalStateException("Pie rule can only be applied on the second turn!");
         }
 
         board.changeStone(firstMovePosition, PlayerColor.WHITE);
 
-        currentTurn=PlayerColor.BLACK;
+        currentTurn = PlayerColor.BLACK;
         turnsCount++;
     }
 
@@ -42,6 +44,44 @@ public class Game {
     }
 
     public PlayerColor getWinner() {
+        PlayerColor winner = null;
+        if (checkWin(PlayerColor.WHITE)) {
+            winner = PlayerColor.WHITE;
+        }
+        if (checkWin(PlayerColor.BLACK)) {
+            winner = PlayerColor.BLACK;
+        }
         return winner;
+    }
+
+    private boolean checkWin(PlayerColor color) {
+        boolean[][] visited = new boolean[19][19];
+        Queue<Position> queue = new LinkedList<>();
+
+        for (int i = 0; i < 19; i++) {
+            Position startPos = (color == PlayerColor.WHITE) ? new Position(0, i) : new Position(i, 0);
+            if (board.getStone(startPos) == color) {
+                queue.add(startPos);
+                visited[startPos.x()][startPos.y()] = true;
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            Position current = queue.poll();
+            if (color == PlayerColor.WHITE && current.x() == 18) {
+                return true;
+            }
+            if (color == PlayerColor.BLACK && current.y() == 18) {
+                return true;
+            }
+
+            for (Position neighbor : board.getAdjacentPositions(current)) {
+                if (!visited[neighbor.x()][neighbor.y()] && board.getStone(neighbor) == color) {
+                    visited[neighbor.x()][neighbor.y()] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+        return false;
     }
 }
